180362bca110ab99e42261f9bbb855c95caa9718
advmaster <118003523+advmaster@users.noreply.github.com>
2023-09-25 15:45:31 -0300

    Update [id].js

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -1,12 +1,12 @@
-import { useRouter } from "next/router";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "../../components/Navbar"
 import Cardapio from "../../components/Cardapio"
 
 export default function Menu() {  
-  const router = useRouter()
-  const id = router.query.id
+
+  const id = "teste"
   return (
     <>
       <Navbar />
